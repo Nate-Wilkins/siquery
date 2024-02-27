8d1ad9d2dd6a1b6364a52a4b3eae78e288efbb83
nate-wilkins <nate-wilkins@code-null.com>
2024-02-27 00:10:42 -0500

    Update '.envrc.sh'

--- .envrc.sh
+++ .envrc.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 function develop() {
-  nix develop --experimental-features 'nix-command flakes' --ignore-environment  . $@
+  nix develop \
+    --experimental-features 'nix-command flakes' \
+    --ignore-environment \
+    . $@
 }
 
 function build() {
-  nix build   --experimental-features 'nix-command flakes' --show-trace --verbose --option eval-cache false . $@
+  nix build \
+    --experimental-features 'nix-command flakes' \
+    --show-trace \
+    --verbose \
+    --option eval-cache false \
+    . $@
 }
